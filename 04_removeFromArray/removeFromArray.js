1fed171f06eec683dbd04aaeabafd0cdcbb9d430
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-15 12:24:44 +0100

    Complete javascript-exercises 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  if(!Array.isArray(arr)){ return 'ERROR'; }
+  let res = [];
+  outer: for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < args.length; j++){
+      if(arr[i] === args[j]){ continue outer; }
+    }
+    res.push(arr[i]);
+  }
+  return res;
 };
 
 // Do not edit below this line
