c21fe6967f2ee164e41500ee9654e5eb579e25dc
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-15 12:49:47 +0100

    Complete javascript-exercises 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * (5 / 9); // Convert to fahrenheit
+  result = Math.floor((result * 10) + 0.5) / 10; // Round to 1.d.p
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * (9 / 5)) + 32; // Convert to celsius
+  result = Math.floor((result * 10) + 0.5) / 10; // Round to 1.d.p
+  return result;
 };
 
 // Do not edit below this line
