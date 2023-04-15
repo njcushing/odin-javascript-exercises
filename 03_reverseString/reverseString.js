2ed08b961e840f1d45aa69a2ab7f8b4a3595a98b
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-15 12:05:45 +0100

    Complete javascript-exercises 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  if(typeof str != "string"){ return 'ERROR'; }
+  let stringArray = str.split("");
+  let result = "";
+  for(let i = stringArray.length - 1; i >= 0; i--){
+    result += stringArray[i];
+  }
+  return result;
 };
 
 // Do not edit below this line
