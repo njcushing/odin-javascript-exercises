aef8619766dc8ed069141ac8e584b577ff2065cb
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-15 12:36:42 +0100

    Complete javascript-exercises 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(one, two) {
+  if(one < 0 || two < 0){ return 'ERROR'; }
+  if(typeof one != "number" || typeof two != "number"){ return 'ERROR'; }
+  let first = Math.min(one, two);
+  let last = Math.max(one, two);
+  let sum = 0;
+  for(let i = first; i <= last; i++){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
