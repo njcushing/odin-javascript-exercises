a3d8c3b99436e3c866a943122b72e532c4f4a5c5
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-19 22:36:39 +0100

    Complete javascript-exercises 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people = people.sort((a, b) => {
+        let aAge = !('yearOfDeath' in a) ?
+                    new Date().getFullYear() - a.yearOfBirth :
+                    a.yearOfDeath - a.yearOfBirth;
+        let bAge = !('yearOfDeath' in b) ?
+                    new Date().getFullYear() - b.yearOfBirth :
+                    b.yearOfDeath - b.yearOfBirth;
+        return (aAge > bAge) ? -1 : 1;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
