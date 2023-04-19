0fc20be14773dab2e5d27415ddfbe5930d1c3620
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-19 21:42:07 +0100

    Complete javascript-exercises 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(one, two) {
+	return one + two;
 };
 
-const subtract = function() {
-	
+const subtract = function(one, two) {
+	return one - two;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+    return numbers.reduce((tot, number) => (tot += number), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	let result = 1;
+    for(let i = 0; i < numbers.length; i++){ result *= numbers[i]; }
+    return result;
 };
 
-const power = function() {
-	
+const power = function(value, power) {
+	return value ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+    let result = 1;
+    for(let i = 1; i <= value; i++){
+        result *= i;
+    }
+    return result;
 };
 
 // Do not edit below this line
