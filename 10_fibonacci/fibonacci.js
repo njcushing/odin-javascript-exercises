7ab7acae0d0873a5294623f301799c622b5c50a7
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-19 22:20:14 +0100

    Complete javascript-exercises 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 0){ return 'OOPS'; }
+    if(index == 0){ return 1; }
+    let prev = 0;
+    let curr = 1;
+    for(let i = 1; i < index; i++){
+        let temp = prev + curr;
+        prev = curr;
+        curr = temp;
+    }
+    return curr;
 };
 
 // Do not edit below this line
